3df2ac39d3c1f98bc053344d5470a6c226e8acfa
Federico Marini <marinif@uni-mainz.de>
2019-06-24 15:33:34 +0200

    R pkg documentation for the R folder

---
+++ R/GeneTonic-pkg.R
@@ -0,0 +1,10 @@
+#' GeneTonic
+#'
+#' \code{GeneTonic} is a Bioconductor package that provides an interactive Shiny-based
+#'  graphical user interface for...
+#'
+#' @author Federico Marini \email{marinif@@uni-mainz.de}
+#' @author Simon Tiado Stahl \email{simon.t.stahl@@gmail.com}
+#' @name GeneTonic-pkg
+#' @docType package
+NULL
