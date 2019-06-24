35589fb9c629c464c99a386b83b0e2d6ddef050c
Federico Marini <marinif@uni-mainz.de>
2019-06-24 17:52:39 +0200

    Added main function and roxygen skeleton

---
+++ R/GeneTonic.R
@@ -0,0 +1,19 @@
+#' GeneTonic
+#'
+#' GeneTonic, main function for the Shiny app
+#'
+#' @param dds dds
+#' @param res result
+#' @param func_enrich functional enrichment
+#'
+#' @return A Shiny app object is returned, for interactive data exploration
+#' @export
+#'
+#' @examples
+GeneTonic <- function(dds,
+                      res,
+                      func_enrich) {
+  # main body of the Shiny app function!
+
+
+}
