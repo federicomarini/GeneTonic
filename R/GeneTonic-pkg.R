5eab21da3179433c55f3fb94df9edd73a5fdfa40
Federico Marini <marinif@uni-mainz.de>
2019-09-26 23:19:35 +0200

    import for roxygen as well

--- R/GeneTonic-pkg.R
+++ R/GeneTonic-pkg.R
@@ -5,6 +5,13 @@
 #'
 #' @author Federico Marini \email{marinif@@uni-mainz.de}
 #' @author Simon Tiado Stahl \email{simon.t.stahl@@gmail.com}
+#'
+#' @import shinydashboard
+#' @importFrom DT dataTableOutput renderDataTable datatable
+#' @importFrom rintrojs introjs introjsUI
+#' @import igraph
+#' @import visNetwork
+#'
 #' @name GeneTonic-pkg
 #' @docType package
 NULL
