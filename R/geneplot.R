bb7fb1534fa97d585a1ca6912eeb7373d2e9469c
Federico Marini <marinif@uni-mainz.de>
2019-10-04 01:23:25 +0200

    single gene plot function day0

---
+++ R/geneplot.R
@@ -0,0 +1,6 @@
+# something on the line of plotCounts, ggplotCounts, but with more pimpedity :D
+## maybe even plotly-fied already, or pimped in gg so that it is readily plugged into ggplotly
+
+geneplot <- function( ) {
+  # yay
+}
