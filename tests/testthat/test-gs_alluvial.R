fac897c6bd80d52ba5286b1ec6f05cbe3d4d8e0d
Federico Marini <marinif@uni-mainz.de>
2019-10-28 14:47:44 +0100

    Removed static alluvial plot - issues with import of ggalluvial while checking/testing in packages

--- tests/testthat/test-gs_alluvial.R
+++ tests/testthat/test-gs_alluvial.R
@@ -1,4 +1,4 @@
-context("Testing the alluvial plots, both interactive and static")
+context("Testing the alluvial plots")
 
 test_that("Alluvial plot (interactive) is generated", {
   p <- gs_alluvial(res_enrich = topgoDE_macrophage_IFNg_vs_naive,
@@ -7,13 +7,5 @@ test_that("Alluvial plot (interactive) is generated", {
                    n_gs = 5)
   expect_is(p, "plotly")
   # TODOTODO, expect I get htmlwidgets...
-})
-
-test_that("Alluvial plot (static) is generated", {
-  p <- gs_alluvial2(res_enrich = topgoDE_macrophage_IFNg_vs_naive,
-                   res_de = res_macrophage_IFNg_vs_naive,
-                   annotation_obj = anno_df,
-                   n_gs = 5)
-  expect_is(p, "gg")
 })
 
