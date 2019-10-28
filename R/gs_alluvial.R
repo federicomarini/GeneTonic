0d4e05b7d64a9411ee3d2f7cfc19d336056766d8
Federico Marini <marinif@uni-mainz.de>
2019-10-28 11:07:07 +0100

    split up into plotly-based and gg-static version

--- R/gs_alluvial.R
+++ R/gs_alluvial.R
@@ -7,7 +7,6 @@
 #' @param genes_colname
 #' @param genesetname_colname
 #' @param genesetid_colname
-#' @param ggstatic
 #'
 #' @return
 #' @export
@@ -19,8 +18,7 @@ gs_alluvial <- function(res_enrich,
                         n_gs = 5,
                         genes_colname = "genes",
                         genesetname_colname = "Term",
-                        genesetid_colname = "GO.ID",
-                        ggstatic = FALSE) {
+                        genesetid_colname = "GO.ID") {
 
   res_enhanced <- get_aggrscores(res_enrich, res_de, annotation_obj = annotation_obj)
 
@@ -47,75 +45,110 @@ gs_alluvial <- function(res_enrich,
 
   mygsdf <- list2df
 
-  if (ggstatic) {
-    mygsdf$value <- 1
 
-    ggplot(as.data.frame(mygsdf),
-           aes(y = value, axis1 = gsid, axis2 = gene)) +
-      geom_alluvium(aes(fill = gsid), width = 1/12) +
-      geom_stratum(width = 1/12, fill = "grey", color = "black") +
-      geom_label(stat = "stratum", label.strata = TRUE) +
-      # geom_label_repel(stat = "stratum", label.strata = TRUE) +
-      scale_x_discrete(limits = c("Gene set", "Gene"), expand = c(0.5, 0.2)) +
-      theme_void() -> p
-    return(p)
-  } else {
 
-    mygsdf2 <- list2df
-    mygsdf2$value <- 1
-    colnames(mygsdf2) <- c("source", "target", "value")
-    mygsdf2$source <- as.character(mygsdf2$source)
-    mygsdf2$target <- as.character(mygsdf2$target)
-    mygsdf2$target <- paste(mygsdf2$target, " ", sep="")
+  mygsdf2 <- list2df
+  mygsdf2$value <- 1
+  colnames(mygsdf2) <- c("source", "target", "value")
+  mygsdf2$source <- as.character(mygsdf2$source)
+  mygsdf2$target <- as.character(mygsdf2$target)
+  mygsdf2$target <- paste(mygsdf2$target, " ", sep="")
 
-    # From these flows we need to create a node data frame: it lists every entities involved in the flow
-    nodes <- data.frame(name=c(as.character(mygsdf2$source), as.character(mygsdf2$target)) %>% unique())
+  # From these flows we need to create a node data frame: it lists every entities involved in the flow
+  nodes <- data.frame(name=c(as.character(mygsdf2$source), as.character(mygsdf2$target)) %>% unique())
 
-    # With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
-    mygsdf2$IDsource=match(mygsdf2$source, nodes$name)-1
-    mygsdf2$IDtarget=match(mygsdf2$target, nodes$name)-1
+  # With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
+  mygsdf2$IDsource=match(mygsdf2$source, nodes$name)-1
+  mygsdf2$IDtarget=match(mygsdf2$target, nodes$name)-1
 
-    mygsdf2 %>% head
+  mygsdf2 %>% head
 
-    allnodes <- c(unique(mygsdf2$source), unique(mygsdf2$target))
+  allnodes <- c(unique(mygsdf2$source), unique(mygsdf2$target))
 
-    library(plotly)
+  library(plotly)
 
-    p <- plot_ly(
-      type = "sankey",
-      domain = list(
-        x =  c(0,1),
-        y =  c(0,1)
-      ),
-      orientation = "h",
-      valueformat = ".0f",
-      valuesuffix = " genes in the set",
+  p <- plot_ly(
+    type = "sankey",
+    domain = list(
+      x =  c(0,1),
+      y =  c(0,1)
+    ),
+    orientation = "h",
+    valueformat = ".0f",
+    valuesuffix = " genes in the set",
 
-      node = list(
-        label = allnodes,
-        color = c(viridis(length(unique(mygsdf2$source))), rep("steelblue",length(unique(mygsdf2$target)))),
-        pad = 15,
-        thickness = 20,
-        line = list(
-          color = "black",
-          width = 0.5
-        )
-      ),
-
-      link = list(
-        source = mygsdf2$IDsource,
-        target = mygsdf2$IDtarget,
-        value =  mygsdf2$value
+    node = list(
+      label = allnodes,
+      color = c(viridis(length(unique(mygsdf2$source))), rep("steelblue",length(unique(mygsdf2$target)))),
+      pad = 15,
+      thickness = 20,
+      line = list(
+        color = "black",
+        width = 0.5
       )
-    ) %>%
-      plotly::layout(
-        title = "Geneset-Gene Sankey Diagram",
-        font = list(
-          size = 10
-        )
+    ),
+
+    link = list(
+      source = mygsdf2$IDsource,
+      target = mygsdf2$IDtarget,
+      value =  mygsdf2$value
+    )
+  ) %>%
+    plotly::layout(
+      title = "Geneset-Gene Sankey Diagram",
+      font = list(
+        size = 10
       )
-    return(p)
-  }
+    )
+  return(p)
+
+
+}
+
+gs_alluvial2 <- function(res_enrich,
+                        res_de,
+                        annotation_obj,
+                        n_gs = 5,
+                        genes_colname = "genes",
+                        genesetname_colname = "Term",
+                        genesetid_colname = "GO.ID") {
+
+  res_enhanced <- get_aggrscores(res_enrich, res_de, annotation_obj = annotation_obj)
+
+  enriched_gsids <- res_enrich[[genesetid_colname]]
+  enriched_gsnames <- res_enrich[[genesetname_colname]]
+  enriched_gsdescs <- vapply(enriched_gsids,
+                             function(arg) Definition(GOTERM[[arg]]),
+                             character(1))
+
+  enrich2list <- lapply(seq_len(n_gs), function(gs) {
+    # goterm <- res_enrich$Term[gs]
+    go_genes <- res_enrich$genes[gs]
+    go_genes <- strsplit(go_genes, ",") %>% unlist
+    return(go_genes)
+  })
+  names(enrich2list) <- enriched_gsnames[seq_len(n_gs)]
+
+  list2df <- lapply(seq_len(length(enrich2list)), function(gs) {
+    data.frame(
+      gsid = rep(names(enrich2list[gs]), length(enrich2list[[gs]])),
+      gene = enrich2list[[gs]])
+  })
+  list2df <- do.call("rbind", list2df)
+
+  mygsdf <- list2df
+
+  mygsdf$value <- 1
+
+  ggplot(as.data.frame(mygsdf),
+         aes(y = value, axis1 = gsid, axis2 = gene)) +
+    geom_alluvium(aes(fill = gsid), width = 1/12) +
+    geom_stratum(width = 1/12, fill = "grey", color = "black") +
+    geom_label(stat = "stratum", label.strata = TRUE) +
+    # geom_label_repel(stat = "stratum", label.strata = TRUE) +
+    scale_x_discrete(limits = c("Gene set", "Gene"), expand = c(0.5, 0.2)) +
+    theme_void() -> p
+  return(p)
 
 }
 
