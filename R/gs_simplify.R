ffd27a3391358e66d4fe4b6dd1ccec196db1f866
Federico Marini <marinif@uni-mainz.de>
2019-11-04 12:20:40 +0100

    Documenting fully gs_simplify

--- R/gs_simplify.R
+++ R/gs_simplify.R
@@ -1,17 +1,26 @@
-
-
-#' Title TODO
+#' Simplify results from functional enrichment analysis
 #'
-#' TODO
+#' Simplify results from functional enrichment analysis, removing genesets that
+#' are redundant to enhance interpretation of the results
 #'
-#' @param res_enrich TODO
-#' @param gs_overlap TODO
-#' @param genes_colname TODO
-#' @param genesetname_colname TODO
-#' @param genesetid_colname TODO
-#' @param genes_separator TODO
+#' @param res_enrich A `data.frame` object, storing the result of the functional
+#' enrichment analysis. See more in the main function, `GeneTonic`, to see the
+#' formatting requirements.
+#' @param gs_overlap Numeric value, which defines the threshold for removing
+#' terms that present an overlap greater than the specified value. Changing its
+#' value can control the granularity of how redundant terms are removed from the
+#' original `res_enrich` for the next steps, e.g. plotting this via `gs_volcano`
+#' @param genes_colname Character, specifying which column of the `res_enrich`
+#' object contains the genes assigned to each gene set, detected as differentially
+#' expressed. Defaults to `genes`.
+#' @param genesetname_colname Character, specifies which column of the `res_enrich`
+#' object contains a description of the gene set. Defaults to `Term`.
+#' @param genesetid_colname Character, specifies which column of the `res_enrich`
+#' object contains a unique identifier of the gene set. Defaults to `GO.ID`.
+#' @param genes_separator Character, specifying which separator is used in the
+#' column defined by `genes_colname` to split the character of features.
 #'
-#' @return TODO
+#' @return A `data.frame` with a subset of the original gene sets
 #' @export
 #'
 #' @examples
