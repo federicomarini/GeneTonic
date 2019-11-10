ae0a5df0113795e601a64d65c7d769428fcac99d
Federico Marini <marinif@uni-mainz.de>
2019-11-10 23:04:15 +0100

    Extended tests for gs_ functions with custom sets

--- tests/testthat/test-gs_mds.R
+++ tests/testthat/test-gs_mds.R
@@ -1,7 +1,7 @@
 context("MDS plot of genesets works")
 
 test_that("mds plot works", {
-  p <- gs_mds(res_enrich = topgoDE_macrophage_IFNg_vs_naive,
+  p <- gs_mds(res_enrich = res_enrich_IFNg_vs_naive,
               res_de = res_macrophage_IFNg_vs_naive,
               annotation_obj = anno_df,
               similarity_measure = "kappa_matrix",
@@ -9,4 +9,22 @@ test_that("mds plot works", {
               mds_labels = 10,
               mds_colorby = "z_score")
   expect_is(p, "gg")
+  myset <- res_enrich_IFNg_vs_naive$gs_id[c(1,10,20)]
 })
+
+test_that("mds plot with specified sets works", {
+  mygenesets <- res_enrich_IFNg_vs_naive$gs_id[c(1,10,20)]
+  p <- gs_mds(res_enrich = res_enrich_IFNg_vs_naive,
+              res_de = res_macrophage_IFNg_vs_naive,
+              annotation_obj = anno_df,
+              gs_labels = mygenesets,
+              mds_colorby = "z_score")
+  expect_is(p, "gg")
+  expect_warning(gs_mds(res_enrich = res_enrich_IFNg_vs_naive,
+                        res_de = res_macrophage_IFNg_vs_naive,
+                        annotation_obj = anno_df,
+                        gs_labels = "a_random_name",
+                        mds_colorby = "z_score"))
+})
+
+
