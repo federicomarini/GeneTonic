5e6ad256d42a9b5628e16628ae6ed54f95fc78da
Federico Marini <marinif@uni-mainz.de>
2019-11-10 23:11:28 +0100

    Input checkers tested

---
+++ tests/testthat/test-check_inputs.R
@@ -0,0 +1,28 @@
+context("Input main parameter checking works")
+
+test_that("Early fails are triggered", {
+
+  # providing a count matrix
+  expect_error(GeneTonic(counts(dds_macrophage),
+                         res_macrophage_IFNg_vs_naive,
+                         res_enrich_IFNg_vs_naive,
+                         annotation_obj = anno_df))
+
+  # providing a simple data frame
+  expect_error(GeneTonic(dds_macrophage,
+                         deseqresult2df(res_macrophage_IFNg_vs_naive),
+                         res_enrich_IFNg_vs_naive,
+                         annotation_obj = anno_df))
+
+  # providing data frame with missing key columns
+  expect_error(GeneTonic(dds_macrophage,
+                         res_macrophage_IFNg_vs_naive,
+                         res_enrich_IFNg_vs_naive[, -1],
+                         annotation_obj = anno_df))
+
+  # providing data frame with missing key columns
+  expect_error(GeneTonic(dds_macrophage,
+                         res_macrophage_IFNg_vs_naive,
+                         res_enrich_IFNg_vs_naive,
+                         annotation_obj = anno_df[,-1]))
+})
