e41760a27487537c66a63f35027b4f2475af04d7
Federico Marini <marinif@uni-mainz.de>
2019-11-11 10:14:45 +0100

    moved general code to setup script

--- tests/testthat/test-gs_shaker.R
+++ tests/testthat/test-gs_shaker.R
@@ -7,19 +7,7 @@ test_that("Converting from topGOtable results", {
 })
 
 test_that("Converting from clusterProfiler", {
-  de_symbols_IFNg_vs_naive <- deseqresult2df(res_macrophage_IFNg_vs_naive, FDR = 0.05)$SYMBOL
-  bg_ids <- rowData(dds_macrophage)$SYMBOL[rowSums(counts(dds_macrophage)) > 0]
-  library("clusterProfiler")
-  library("org.Hs.eg.db")
-  ego_IFNg_vs_naive <- enrichGO(gene = de_symbols_IFNg_vs_naive,
-                                universe      = bg_ids,
-                                keyType = "SYMBOL",
-                                OrgDb         = org.Hs.eg.db,
-                                ont           = "BP",
-                                pAdjustMethod = "BH",
-                                pvalueCutoff  = 0.01,
-                                qvalueCutoff  = 0.05,
-                                readable      = FALSE)
+
   res_enrich_IFNg_vs_naive_cp <- shake_enrichResult(ego_IFNg_vs_naive)
   required_colnames <- c("gs_id", "gs_description", "gs_pvalue", "gs_genes", "gs_de_count", "gs_bg_count")
   expect_true(all(required_colnames %in% colnames(res_enrich_IFNg_vs_naive_cp)))
