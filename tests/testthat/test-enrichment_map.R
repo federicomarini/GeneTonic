032395abdf86e2627f62f8c69a60186216eb0d50
Federico Marini <marinif@uni-mainz.de>
2020-01-24 08:19:29 +0100

    Extra tests: enrichment_map

--- tests/testthat/test-enrichment_map.R
+++ tests/testthat/test-enrichment_map.R
@@ -7,6 +7,26 @@ test_that("Graph is generated", {
                       n_gs = 50)
   expect_is(g, "igraph")
 
+  res_enrich_withscores <- get_aggrscores(res_enrich_IFNg_vs_naive,
+                                          res_macrophage_IFNg_vs_naive,
+                                          annotation_obj = anno_df,
+                                          aggrfun = mean)
+  g2 <- enrichment_map(res_enrich = res_enrich_withscores,
+                      res_de = res_macrophage_IFNg_vs_naive,
+                      annotation_obj = anno_df,
+                      color_by = "z_score",
+                      n_gs = 50)
+  expect_is(g2, "igraph")
+
+  res_pos_z <- res_enrich_withscores[res_enrich_withscores$z_score >= 0, ]
+  g3 <- enrichment_map(res_enrich = res_pos_z,
+                       res_de = res_macrophage_IFNg_vs_naive,
+                       annotation_obj = anno_df,
+                       color_by = "z_score",
+                       n_gs = 50)
+  expect_is(g3, "igraph")
+
+
   library(magrittr)
   vi <- visNetwork::visIgraph(g) %>%
     visOptions(highlightNearest = list(enabled = TRUE,
@@ -16,4 +36,14 @@ test_that("Graph is generated", {
 
   expect_is(vi, "visNetwork")
   expect_is(vi, "htmlwidget")
+})
+
+test_that("Errors and warnings are thrown", {
+  expect_error(
+    enrichment_map(res_enrich = res_enrich_IFNg_vs_naive,
+                   res_de = res_macrophage_IFNg_vs_naive,
+                   annotation_obj = anno_df,
+                   color_by = "pvalue",
+                   n_gs = 50)
+  )
 })
