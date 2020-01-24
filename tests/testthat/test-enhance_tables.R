44a40e139a1e68189240e1559a2af467a2bdd9c4
Federico Marini <marinif@uni-mainz.de>
2020-01-24 08:19:17 +0100

    Extra tests: enhance_table

--- tests/testthat/test-enhance_tables.R
+++ tests/testthat/test-enhance_tables.R
@@ -11,4 +11,14 @@ test_that("Enhanced table is created", {
   pl <- ggplotly(p)
   expect_is(pl, "plotly")
   expect_is(pl, "htmlwidget")
+
+  p2 <- enhance_table(res_enrich_IFNg_vs_naive,
+                     res_macrophage_IFNg_vs_naive,
+                     annotation_obj = anno_df,
+                     n_gs = 5,
+                     chars_limit = 60,
+                     plot_title = "My custom title")
+  expect_is(p2, "gg")
+
+
 })
