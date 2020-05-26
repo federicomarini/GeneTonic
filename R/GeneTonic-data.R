198fef135671d2f34225d0bc96ee289bc2e62447
Federico Marini <marinif@uni-mainz.de>
2020-05-26 10:31:01 +0200

    Documenting data object from Enrichr

--- R/GeneTonic-data.R
+++ R/GeneTonic-data.R
@@ -38,3 +38,31 @@ NULL
 #' @name topgoDE_macrophage_IFNg_vs_naive
 #' @docType data
 NULL
+
+
+#' A sample output from Enrichr
+#'
+#' A sample output object as created from a call to Enrichr, with the interface
+#' provided by `enrichR` - using the `enrichr()` function
+#'
+#' @details This object has been created on the data from the `macrophage` package
+#' by analyzing downstream the differentially expressed genes
+#' when comparing IFNg treated samples vs naive samples, accounting
+#' for the different cell lines included.
+#'
+#' Details on how this object has been created are included in the `create_gt_data.R`
+#' script, included in the `scripts` folder of the `GeneTonic` package.
+#'
+#' @references Alasoo, et al. "Shared genetic effects on chromatin and gene
+#' expression indicate a role for enhancer priming in immune response",
+#' Nature Genetics, January 2018 doi: 10.1038/s41588-018-0046-7.
+#'
+#' @name enrichr_output_macrophage
+#' @docType data
+NULL
+
+
+
+
+
+
