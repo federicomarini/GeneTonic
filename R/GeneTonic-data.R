0d8b56c852eb748c979fa815e4f215fb16d08c82
Federico Marini <marinif@uni-mainz.de>
2020-05-28 10:15:23 +0200

    added info for fgsea output sample data

--- R/GeneTonic-data.R
+++ R/GeneTonic-data.R
@@ -90,3 +90,27 @@ NULL
 #' @docType data
 NULL
 
+
+#' A sample output from fgsea
+#'
+#' A sample output object as created from a call to the `fgsea()` function, in
+#' the `fgsea` package, as a practical framework for performing GSEA
+#'
+#' @details This object has been created on the data from the `macrophage` package
+#' by analyzing downstream the differentially expressed genes
+#' when comparing IFNg treated samples vs naive samples, accounting
+#' for the different cell lines included.
+#'
+#' Details on how this object has been created are included in the `create_gt_data.R`
+#' script, included in the `scripts` folder of the `GeneTonic` package.
+#'
+#' @references Alasoo, et al. "Shared genetic effects on chromatin and gene
+#' expression indicate a role for enhancer priming in immune response",
+#' Nature Genetics, January 2018 doi: 10.1038/s41588-018-0046-7.
+#'
+#' @name fgseaRes
+#' @docType data
+NULL
+
+
+
