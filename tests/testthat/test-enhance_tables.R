f9c22672f76c55113a18518734d6fbb11c4d3053
Federico Marini <marinif@uni-mainz.de>
2020-06-26 01:37:11 +0200

    first sets for distill_enrichment

--- tests/testthat/test-enhance_tables.R
+++ tests/testthat/test-enhance_tables.R
@@ -13,12 +13,24 @@ test_that("Enhanced table is created", {
   expect_is(pl, "htmlwidget")
 
   p2 <- enhance_table(res_enrich_IFNg_vs_naive,
-                     res_macrophage_IFNg_vs_naive,
-                     annotation_obj = anno_df,
-                     n_gs = 5,
-                     chars_limit = 60,
-                     plot_title = "My custom title")
+                      res_macrophage_IFNg_vs_naive,
+                      annotation_obj = anno_df,
+                      n_gs = 5,
+                      chars_limit = 60,
+                      plot_title = "My custom title")
   expect_is(p2, "gg")
 
 
+})
+
+test_that("The distillery is up and running", {
+  d_en <- distill_enrichment(res_enrich_IFNg_vs_naive,
+                             res_macrophage_IFNg_vs_naive,
+                             annotation_obj = anno_df,
+                             n_gs = 100,
+                             cluster_fun = "cluster_markov")
+  
+  expect_is(d_en, "list")
+  expect_is(d_en$distilled_table, "data.frame")
+  expect_is(d_en$distilled_em, "igraph")
 })
