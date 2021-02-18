db1e8aa8742ebdce4c0fb7e9d952045fd6d3b370
Ludt <anneludt@Uni-Mainz.De>
2021-02-18 10:49:03 +0100

    fixed RMD check for branch

--- R/ggs_volcano.R
+++ R/ggs_volcano.R
@@ -17,7 +17,7 @@
 #' @param FDR Numeric value, specifying the significance level for thresholding
 #' adjusted p-values. Defaults to 0.05.
 #' @param color Character string to specify color of filtered points in the plot.
-#'  Default color is red.
+#'  Default color is #0092AC.
 #' @param volcano_labels Integer, maximum number of labels for the gene sets to be
 #' plotted as labels on the volcano scatter plot.
 #' @param plot_title Character string, to specify the title of the plot,
@@ -39,7 +39,6 @@
 #' rownames(dds_macrophage) <- substr(rownames(dds_macrophage), 1, 15)
 #' dds_macrophage <- estimateSizeFactors(dds_macrophage)
 #'
-#' vst_macrophage <- vst(dds_macrophage)
 #'
 #' # annotation object
 #' anno_df <- data.frame(
@@ -61,8 +60,7 @@
 #' res_enrich <- shake_topGOtableResult(topgoDE_macrophage_IFNg_vs_naive)
 #' res_enrich <- get_aggrscores(res_enrich, res_de, anno_df)
 #'
-#' ggs_volcano(vst_macrophage,
-#'            res_de,
+#' ggs_volcano(res_de,
 #'            res_enrich,
 #'            anno_df,
 #'            geneset_id = res_enrich$gs_id[1]
@@ -76,7 +74,7 @@ ggs_volcano <- function(res_de,
                        genelist = NULL,
                        FDR = 0.05,
                        color = "red",
-                       volcano_labels = 10,
+                       volcano_labels = 25,
                        plot_title = NULL
                        ) {
 
@@ -96,27 +94,48 @@ ggs_volcano <- function(res_de,
       thisset_members_ids <- annotation_obj$gene_id[match(thisset_members, annotation_obj$gene_name)]
     }
   }else {
-    # overridable via a list
-    if (!all(genelist %in% rownames(se))) {
-      not_there <- genelist[!(genelist %in% rownames(se))]
+    # overwritable via a list
+    if (!all(genelist %in% res_de@rownames)) {
+      not_there <- genelist[!(genelist %in% res_de@rownames)]
       warning("Some of the provided gene ids were not found in the SummarizedExperiment",
               "\nNot found: ", not_there)
     }
-    thisset_members_ids <- intersect(rownames(se), genelist)
+    thisset_members_ids <- intersect(res_de@rownames, genelist)
     thisset_name <- "Custom list"
   }
 
   thisset_members_ids
   thisset_members
   
+  complete_genes_ids <- res_de@rownames
+  complete_genes <- annotation_obj$gene_name[match(complete_genes_ids, annotation_obj$gene_id)]
+  padj_complete <- res_de[complete_genes_ids, "padj"]
+  filter_info_complete <- sapply(padj_complete, function(x) x <= FDR)
+  padj_complete <- sapply(padj_complete, function(x) -log10(x))
+  log2FoldChange_complete <- res_de[complete_genes_ids, "log2FoldChange"]
+  gene_set_belong <- complete_genes_ids %in% thisset_members_ids
+  
+  filter_info_complete <- filter_info_complete & gene_set_belong
+
+  
+  thisset_complete_data <- data.frame(complete_genes_ids, padj_complete, log2FoldChange_complete, filter_info_complete, gene_set_belong)
+  colnames(thisset_complete_data) <- c("genes", "logTransformedpvalue", "log2FoldChange", "significant", "belonging")
+  
+  
+  # Prepare plotting
+  volcano_df_complete <- thisset_complete_data
+  volcano_df_complete$genes_name <- complete_genes
+  max_x <- max(abs(range(thisset_complete_data["log2FoldChange"])))
+  limit_x <- max_x * c(-1, 1)
+  
   # retrieve adjusted pvalues and log2Fold Change for genes
-  padj <- res_de[thisset_members_ids, "padj"]
-  padjlog <- sapply(padj, function(x) -log10(x))
-  log2FoldChange <- res_de[thisset_members_ids, "log2FoldChange"]
-  filter_info <- sapply(padj, function(x) x <= FDR)
+  #padj <- res_de[thisset_members_ids, "padj"]
+  #padjlog <- sapply(padj, function(x) -log10(x))
+  #log2FoldChange <- res_de[thisset_members_ids, "log2FoldChange"]
+  #filter_info <- sapply(padj, function(x) x <= FDR)
   
   # Prepare data to plot
-  thisset_data <- data.frame(thisset_members_ids, padj, log2FoldChange, padjlog, filter_info)
+  #thisset_data <- data.frame(thisset_members_ids, padj, log2FoldChange, padjlog, filter_info)
 
   
   if (is.null(plot_title)) {
@@ -125,30 +144,55 @@ ggs_volcano <- function(res_de,
     title <- plot_title
   }
   
-  colnames(thisset_data) <- c("genes", "pvalue", "log2FoldChange", "logTransformedpvalue", "significant")
+ # colnames(thisset_data) <- c("genes", "pvalue", "log2FoldChange", "logTransformedpvalue", "significant")
   
   
   # Prepare plotting
-  volcano_df <- thisset_data
-  volcano_df$genes_name <- thisset_members
-  max_x <- max(abs(range(thisset_data["log2FoldChange"])))
-  limit_x <- max_x * c(-1, 1)
+ # volcano_df <- thisset_data
+  #volcano_df$genes_name <- thisset_members
+  #max_x <- max(abs(range(thisset_data["log2FoldChange"])))
+  #limit_x <- max_x * c(-1, 1)
 
 
   # Plot data
-   p <- ggplot(volcano_df, aes(x = log2FoldChange, y = logTransformedpvalue, label = genes_name)) +
-    geom_point(aes(color = significant)) +
+  #p <- ggplot(volcano_df, aes(x = log2FoldChange, y = logTransformedpvalue, label = genes_name)) +
+  #geom_point(aes(color = significant)) +
+  #  labs(x = "log2Fold Change",
+  #       y = "-log10 p-value", color = "p-value <= FDR") + 
+  #  scale_x_continuous(limits = limit_x) +
+  #  scale_color_manual(labels = c("significant", "not significant"), 
+  #                     breaks = c("TRUE", "FALSE"), values = c(color, "grey25")) +
+  #  theme(legend.title = element_text(size = 11, face = "bold"), legend.text = element_text(size = 10)) +
+  #  geom_text_repel()
+  
+  p <- ggplot(volcano_df_complete, aes_string(x = "log2FoldChange", y = "logTransformedpvalue")) +
+    geom_point(aes_string(color = "significant", alpha = "belonging")) +
     labs(x = "log2Fold Change",
-         y = "-log10 p-value", color = "p-value <= FDR") + 
+         y = "-log10 p-value",
+         color = "p-value <= FDR") + 
     scale_x_continuous(limits = limit_x) +
     scale_color_manual(labels = c("significant", "not significant"), 
-                       breaks = c("TRUE", "FALSE"), values = c(color, "grey25")) +
-    theme(legend.title = element_text(size = 11, face = "bold"), legend.text = element_text(size = 10)) +
-    geom_text_repel()
+                       breaks = c("TRUE", "FALSE"),
+                       values = c(color, "grey25")) +
+    scale_alpha_manual(breaks = c("TRUE", "FALSE"),
+                       values = c(1, 1/10)) +
+    theme_bw() +
+    theme(legend.title = element_text(size = 11, face = "bold"),
+          legend.text = element_text(size = 10))  
+    
 
     
+  # adding labels to the significant points of the geneset
+  p <- p + geom_text_repel(
+    data = subset(volcano_df_complete, filter_info_complete),
+    aes_string(label = "genes_name"),
+    size = 4,
+    max.overlaps = volcano_labels)
+
+
   
   # handling the title
   p <- p + ggtitle(title)
+  p <- p + guides(alpha = FALSE)
   return(p)
 }
