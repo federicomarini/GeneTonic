cad471aea978475d8e21568f45a9fea536c13d1e
Ludt <anneludt@Uni-Mainz.De>
2021-02-19 17:36:42 +0100

    Added checkbox fr display of all labels

--- R/ggs_volcano.R
+++ R/ggs_volcano.R
@@ -17,9 +17,9 @@
 #' @param FDR Numeric value, specifying the significance level for thresholding
 #' adjusted p-values. Defaults to 0.05.
 #' @param color Character string to specify color of filtered points in the plot.
-#'  Default color is #0092AC.
+#'  Default color is #1a81c2.
 #' @param volcano_labels Integer, maximum number of labels for the gene sets to be
-#' plotted as labels on the volcano scatter plot.
+#' plotted as labels on the volcano scatter plot. Default value is 25.
 #' @param plot_title Character string, to specify the title of the plot,
 #' displayed over the heatmap. If left to `NULL` as by default, it tries to use
 #' the information on the geneset identifier provided
@@ -65,7 +65,17 @@
 #'            anno_df,
 #'            geneset_id = res_enrich$gs_id[1]
 #'            )
+#'            
+#' alternatively
 #'
+#' # genelist <- sapply(anno_df$gene_name, function(x) str_contains(x, "CD"))
+#' 
+#' # ggs_volcano(res_de,
+#' #             res_enrich,
+#' #             anno_df,
+#' #             genelist = genelist
+#' #             )
+#' 
 ggs_volcano <- function(res_de,
                        res_enrich,
                        annotation_obj = NULL,
@@ -95,26 +105,28 @@ ggs_volcano <- function(res_de,
     }
   }else {
     # overwritable via a list
-    if (!all(genelist %in% res_de@rownames)) {
-      not_there <- genelist[!(genelist %in% res_de@rownames)]
+    if (!all(genelist %in% rownames(res_de))) {
+      not_there <- genelist[!(genelist %in% rownames(res_de))]
       warning("Some of the provided gene ids were not found in the SummarizedExperiment",
               "\nNot found: ", not_there)
     }
-    thisset_members_ids <- intersect(res_de@rownames, genelist)
+    thisset_members_ids <- intersect(rownames(res_de), genelist)
     thisset_name <- "Custom list"
   }
 
   thisset_members_ids
-  thisset_members
   
-  complete_genes_ids <- res_de@rownames
+  # Prepare the data
+  complete_genes_ids <- rownames(res_de)
   complete_genes <- annotation_obj$gene_name[match(complete_genes_ids, annotation_obj$gene_id)]
+  
   padj_complete <- res_de[complete_genes_ids, "padj"]
   filter_info_complete <- sapply(padj_complete, function(x) x <= FDR)
   padj_complete <- sapply(padj_complete, function(x) -log10(x))
-  log2FoldChange_complete <- res_de[complete_genes_ids, "log2FoldChange"]
-  gene_set_belong <- complete_genes_ids %in% thisset_members_ids
   
+  log2FoldChange_complete <- res_de[complete_genes_ids, "log2FoldChange"]
+  
+  gene_set_belong <- complete_genes_ids %in% thisset_members_ids
   filter_info_complete <- filter_info_complete & gene_set_belong
 
   
@@ -128,67 +140,38 @@ ggs_volcano <- function(res_de,
   max_x <- max(abs(range(thisset_complete_data["log2FoldChange"])))
   limit_x <- max_x * c(-1, 1)
   
-  # retrieve adjusted pvalues and log2Fold Change for genes
-  #padj <- res_de[thisset_members_ids, "padj"]
-  #padjlog <- sapply(padj, function(x) -log10(x))
-  #log2FoldChange <- res_de[thisset_members_ids, "log2FoldChange"]
-  #filter_info <- sapply(padj, function(x) x <= FDR)
-  
-  # Prepare data to plot
-  #thisset_data <- data.frame(thisset_members_ids, padj, log2FoldChange, padjlog, filter_info)
 
-  
+  # Prepare plot title
   if (is.null(plot_title)) {
     title <- paste0("Volcano Plot - ", thisset_name, " - ", geneset_id)
   } else {
     title <- plot_title
   }
-  
- # colnames(thisset_data) <- c("genes", "pvalue", "log2FoldChange", "logTransformedpvalue", "significant")
-  
-  
-  # Prepare plotting
- # volcano_df <- thisset_data
-  #volcano_df$genes_name <- thisset_members
-  #max_x <- max(abs(range(thisset_data["log2FoldChange"])))
-  #limit_x <- max_x * c(-1, 1)
 
 
   # Plot data
-  #p <- ggplot(volcano_df, aes(x = log2FoldChange, y = logTransformedpvalue, label = genes_name)) +
-  #geom_point(aes(color = significant)) +
-  #  labs(x = "log2Fold Change",
-  #       y = "-log10 p-value", color = "p-value <= FDR") + 
-  #  scale_x_continuous(limits = limit_x) +
-  #  scale_color_manual(labels = c("significant", "not significant"), 
-  #                     breaks = c("TRUE", "FALSE"), values = c(color, "grey25")) +
-  #  theme(legend.title = element_text(size = 11, face = "bold"), legend.text = element_text(size = 10)) +
-  #  geom_text_repel()
-  
   p <- ggplot(volcano_df_complete, aes_string(x = "log2FoldChange", y = "logTransformedpvalue")) +
-    geom_point(aes_string(color = "significant", alpha = "belonging")) +
-    labs(x = "log2Fold Change",
-         y = "-log10 p-value",
-         color = "p-value <= FDR") + 
-    scale_x_continuous(limits = limit_x) +
-    scale_color_manual(labels = c("significant", "not significant"), 
-                       breaks = c("TRUE", "FALSE"),
-                       values = c(color, "grey25")) +
-    scale_alpha_manual(breaks = c("TRUE", "FALSE"),
-                       values = c(1, 1/10)) +
-    theme_bw() +
-    theme(legend.title = element_text(size = 11, face = "bold"),
-          legend.text = element_text(size = 10))  
+       geom_point(aes_string(color = "significant", alpha = "belonging")) +
+       labs(x = "log2Fold Change",
+            y = "-log10 p-value",
+            color = "p-value <= FDR") + 
+       scale_x_continuous(limits = limit_x) +
+       scale_color_manual(labels = c("significant", "not significant"), 
+                          breaks = c("TRUE", "FALSE"),
+                          values = c(color, "grey25")) +
+       scale_alpha_manual(breaks = c("TRUE", "FALSE"),
+                          values = c(1, 1/10)) +
+       theme_bw() +
+       theme(legend.title = element_text(size = 11, face = "bold"),
+             legend.text = element_text(size = 10))  
     
-
-    
+  
   # adding labels to the significant points of the geneset
   p <- p + geom_text_repel(
     data = subset(volcano_df_complete, filter_info_complete),
     aes_string(label = "genes_name"),
     size = 4,
     max.overlaps = volcano_labels)
-
 
   
   # handling the title
