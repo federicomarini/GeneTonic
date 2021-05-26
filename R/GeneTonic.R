3cfbc5f837d4abccc981af71ff5285eb294ae6e4
Federico Marini <marinif@uni-mainz.de>
2021-05-27 01:08:00 +0200

    manually enforcing status for the header

--- R/GeneTonic.R
+++ R/GeneTonic.R
@@ -131,6 +131,7 @@ GeneTonic <- function(dds,
         image = "GeneTonic/GeneTonic.png"
       ),
       skin = "dark",
+      status = "gray-dark",
       border = FALSE,
       controlbarIcon = icon("gears"),
       fixed = TRUE,
