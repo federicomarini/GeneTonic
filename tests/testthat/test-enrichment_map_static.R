4ccd283dbfc5d0defe1e784478cdbeae3fdb672e
Federico Marini <marinif@uni-mainz.de>
2025-03-12 18:54:31 +0100

    adjusting the unit tests for the emap plotting

--- tests/testthat/test-enrichment_map_static.R
+++ tests/testthat/test-enrichment_map_static.R
@@ -1,11 +1,11 @@
-context("Testing enrichmentmap and related functionality")
+context("Testing enrichment map and related functionality")
 
 test_that("Graph is generated", {
-  g <- enrichment_map_static(
+  g <- enrichment_map(
     res_enrich = res_enrich_IFNg_vs_naive,
     res_de = res_macrophage_IFNg_vs_naive,
     annotation_obj = anno_df,
-    n_gs = 100
+    n_gs = 50
   )
   expect_is(g, "igraph")
 
@@ -15,7 +15,7 @@ test_that("Graph is generated", {
     res_enrich = res_enrich_IFNg_vs_naive,
     annotation_obj = anno_df
   )
-  g1 <- enrichment_map_static(gtl = gtl_macrophage)
+  g1 <- enrichment_map(gtl = gtl_macrophage)
   expect_is(g1, "igraph")
 
   expect_true(identical_graphs(g, g1))
@@ -25,7 +25,7 @@ test_that("Graph is generated", {
     annotation_obj = anno_df,
     aggrfun = mean
   )
-  g2 <- enrichment_map_static(
+  g2 <- enrichment_map(
     res_enrich = res_enrich_withscores,
     res_de = res_macrophage_IFNg_vs_naive,
     annotation_obj = anno_df,
@@ -33,41 +33,13 @@ test_that("Graph is generated", {
     n_gs = 50
   )
   expect_is(g2, "igraph")
-
-  res_pos_z <- na.omit(res_enrich_withscores[res_enrich_withscores$z_score >= 0, ])
-  g3 <- enrichment_map_static(
-    res_enrich = res_pos_z,
-    res_de = res_macrophage_IFNg_vs_naive,
-    annotation_obj = anno_df,
-    color_by = "z_score",
-    n_gs = 50
+  
+  gp <- plot_emap_static(
+    emg = g2
   )
-  expect_is(g3, "igraph")
-
-
-  library(magrittr)
-  vi <- visNetwork::visIgraph(g) %>%
-    visOptions(
-      highlightNearest = list(
-        enabled = TRUE,
-        degree = 1,
-        hover = TRUE
-      ),
-      nodesIdSelection = TRUE
-    )
-
-  expect_is(vi, "visNetwork")
-  expect_is(vi, "htmlwidget")
-})
-
-test_that("Errors and warnings are thrown", {
-  expect_error(
-    enrichment_map_static(
-      res_enrich = res_enrich_IFNg_vs_naive,
-      res_de = res_macrophage_IFNg_vs_naive,
-      annotation_obj = anno_df,
-      color_by = "pvalue",
-      n_gs = 50
-    )
-  )
+  
+  expect_is(gp, "ggraph")
+  expect_is(gp, "gg")
+  
+  
 })
