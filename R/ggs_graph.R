ce7056b287521c39405de37f84d8aba65f7102f3
Federico Marini <marinif@uni-mainz.de>
2025-09-23 17:20:24 +0200

    updating dispatch and processing

--- R/ggs_graph.R
+++ R/ggs_graph.R
@@ -399,21 +399,39 @@ ggs_backbone <- function(res_enrich,
     bpm_for_backbone <- t(bpm)
   }
 
-  if (bb_method == "sdsm") {
-    bbobj <- backbone::sdsm(bpm_for_backbone, alpha = NULL)
-  } else if (bb_method == "fdsm") {
-    bbobj <- backbone::fdsm(bpm_for_backbone, trials = 1000, alpha = NULL)
-  } else if (bb_method == "fixedrow") {
-    bbobj <- backbone::fixedrow(bpm_for_backbone, alpha = NULL)
+  # if (bb_method == "sdsm") {
+  #   bbobj <- backbone::sdsm(bpm_for_backbone, alpha = NULL)
+  # } else if (bb_method == "fdsm") {
+  #   bbobj <- backbone::fdsm(bpm_for_backbone, trials = 1000, alpha = NULL)
+  # } else if (bb_method == "fixedrow") {
+  #   bbobj <- backbone::fixedrow(bpm_for_backbone, alpha = NULL)
+  # }
+  # 
+  # bbextracted <- backbone::backbone.extract(bbobj,
+  #   alpha = bb_extract_alpha,
+  #   mtc = bb_extract_fwer
+  # )
+  # 
+  # bbgraph <- igraph::graph_from_adjacency_matrix(bbextracted, mode = "undirected")
+  # 
+  
+  if (bb_method == "fdsm") {
+    bb_trials <- 1000
+  } else {
+    bb_trials <- NULL
   }
-
-  bbextracted <- backbone::backbone.extract(bbobj,
-    alpha = bb_extract_alpha,
-    mtc = bb_extract_fwer
-  )
-
+    
+  bbextracted <- 
+    backbone::backbone_from_projection(
+      B = bpm_for_backbone,
+      alpha = bb_extract_alpha,
+      model = bb_method,
+      mtc = bb_extract_fwer,
+      trials = bb_trials
+    )
+  
   bbgraph <- igraph::graph_from_adjacency_matrix(bbextracted, mode = "undirected")
-
+  
   if (bb_remove_singletons) {
     bbgraph <- igraph::delete_vertices(bbgraph, !(igraph::degree(bbgraph) >= 1))
   }
