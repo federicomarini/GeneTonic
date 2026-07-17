f685b768b57f9e6dd7c2fe5579dc7eb4785d07be
Federico Marini <marinif@uni-mainz.de>
2026-07-17 11:36:09 +0200

    restructured the importFrom statements, each taking up one line

--- R/GeneTonic-pkg.R
+++ R/GeneTonic-pkg.R
@@ -14,12 +14,7 @@
 #'
 #' @importFrom AnnotationDbi Definition GOID Ontology Secondary Synonym Term
 #' @importFrom backbone backbone_from_projection
-#' @importFrom bs4Dash bs4Card bs4DashBody bs4DashControlbar
-#' bs4DashFooter bs4DashNavbar bs4DashPage bs4DashSidebar
-#' bs4InfoBox bs4InfoBoxOutput bs4SidebarMenu
-#' bs4SidebarMenuItem bs4TabItem bs4TabItems bs4ValueBox
-#' bs4ValueBoxOutput renderbs4InfoBox renderbs4ValueBox
-#' bs4TabCard
+#' @importFrom bs4Dash bs4Card bs4DashBody bs4DashControlbar bs4DashFooter bs4DashNavbar bs4DashPage bs4DashSidebar bs4InfoBox bs4InfoBoxOutput bs4SidebarMenu bs4SidebarMenuItem bs4TabItem bs4TabItems bs4ValueBox bs4ValueBoxOutput renderbs4InfoBox renderbs4ValueBox bs4TabCard
 #' @importFrom circlize colorRamp2
 #' @importFrom colorspace rainbow_hcl
 #' @importFrom colourpicker colourInput
@@ -28,8 +23,7 @@
 #' @importFrom dendextend branches_attr_by_clusters set
 #' @importFrom DESeq2 vst counts estimateSizeFactors normalizationFactors sizeFactors
 #' @importFrom dplyr arrange desc group_by mutate pull slice select "%>%"
-#' @importFrom DT datatable dataTableOutput renderDataTable formatRound
-#' formatStyle JS
+#' @importFrom DT datatable dataTableOutput renderDataTable formatRound formatStyle JS
 #' @importFrom dynamicTreeCut cutreeDynamic
 #' @importFrom expm "%^%"
 #' @importFrom ggforce geom_sina
@@ -40,9 +34,7 @@
 #' @importFrom graphics par plot
 #' @importFrom grDevices colorRampPalette rgb col2rgb
 #' @importFrom grid gpar
-#' @importFrom igraph add_edges as_adjacency_matrix as_biadjacency_matrix degree
-#' delete_edges delete_vertices "%du%" E "E<-" graph_from_data_frame induced_subgraph
-#' make_full_graph permute strength V "V<-" vcount as_edgelist
+#' @importFrom igraph add_edges as_adjacency_matrix as_biadjacency_matrix degree delete_edges delete_vertices "%du%" E "E<-" graph_from_data_frame induced_subgraph make_full_graph permute strength V "V<-" vcount as_edgelist
 #' @importFrom matrixStats rowSds
 #' @importFrom methods is
 #' @importFrom plotly ggplotly plotlyOutput renderPlotly plot_ly layout add_trace
@@ -56,20 +48,15 @@
 #' @importFrom shinyAce aceEditor
 #' @importFrom shinycssloaders withSpinner
 #' @importFrom shinyWidgets dropdownButton tooltipOptions
-#' @importFrom stats var dist hclust as.dendrogram as.dist cmdscale median
-#' order.dendrogram runif na.omit
+#' @importFrom stats var dist hclust as.dendrogram as.dist cmdscale median order.dendrogram runif na.omit
 #' @import SummarizedExperiment
 #' @importFrom tidyr separate_rows pivot_longer
 #' @importFrom tippy with_tippy
 #' @importFrom tools file_ext file_path_sans_ext
-#' @importFrom utils read.delim sessionInfo browseURL citation data write.table
-#' head packageDescription
-#' @importFrom visNetwork renderVisNetwork visExport visIgraph visNetworkOutput
-#' visOptions
+#' @importFrom utils read.delim sessionInfo browseURL citation data write.table head packageDescription
+#' @importFrom visNetwork renderVisNetwork visExport visIgraph visNetworkOutput visOptions
 #' @importFrom viridis viridis
-#' @importFrom mosdef deresult_to_df gene_plot geneinfo_to_html go_to_html
-#' map_to_color styleColorBar_divergent create_link_GO create_link_NCBI
-#' create_link_GeneCards create_link_GO 
+#' @importFrom mosdef deresult_to_df gene_plot geneinfo_to_html go_to_html map_to_color styleColorBar_divergent create_link_GO create_link_NCBI create_link_GeneCards create_link_GO 
 #'
 #' @name GeneTonic-pkg
 #' @keywords internal
